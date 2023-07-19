dfd3be3856ed73826ff733c3d2284623f39d8b9f
newprice247 <newprice247@gmail.com>
2023-07-19 10:15:59 -0400

    refactor

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,11 @@
-var question = document.getElementById(`question`)
-// console.log(question)
 
-// var quizButtons = document.querySelectorAll(`.quiz-buttons`);
-// // console.log(quizButtons)
 
 var timer = document.getElementById(`timer`);
 
-var b1 = document.querySelector(`.b1`)
-var b2 = document.querySelector(`.b2`)
-var b3 = document.querySelector(`.b3`)
-var b4 = document.querySelector(`.b4`)
+// var b1 = document.querySelector(`.b1`)
+// var b2 = document.querySelector(`.b2`)
+// var b3 = document.querySelector(`.b3`)
+// var b4 = document.querySelector(`.b4`)
 
 // var answerResult = document.getElementById(`answer-result`)
 
@@ -21,6 +17,8 @@ console.log(quizBox)
 
 // var scoreLink = document.getElementById("score-link");
 // console.log(scoreLink)
+var quizButtons = document.querySelectorAll(`.quiz-buttons`);
+    console.log(quizButtons)
 
 const quiz = [
     {
@@ -45,32 +43,36 @@ const quiz = [
     }
 ];
 
-console.log(quiz[1].options[0])
-var correctAnswer;
+// console.log()
+// var correctAnswer;
 
-var started = true;
+// var score = 0;
 
-var score = 0;
+quizButtons.forEach(element => {
+    element.setAttribute(`style`, `display: none;`)
+})
+b1.removeAttribute(`style`, `display: none;`)
 
-var startQuiz = () => {
-    for (i = 0; i < quiz.length; i++) {
-        console.log(quiz[i])
-        quizBox.innerHTML = `
-        <h1 id="question">${quiz[i].question}</h1>
-                <button class="b1" id="">${quiz[i].options[0]}</button>
-                <button class="b2" id="">${quiz[i].options[1]}</button>
-                <button class="b3" id="">${quiz[i].options[2]}</button>
-                <button class="b4" id="">${quiz[i].options[3]}</button>
-                <p id="answer-result"></p>
-        `
-        b1.addEventListener(`click`, function () {
-            if (correctAnswer) {
-                score += 25;
-            }
-        }
-        )
-    }
+function test(q,i) {
+
+    console.log(`quiz started`)
+    var quizQuestion = document.getElementById(`quiz-question`)
+    console.log(quizQuestion)
+    quizQuestion.textContent = q[i].question;
+
+    quizButtons.forEach(function(element, index) {
+        element.removeAttribute(`style`, `display: none;`)
+        element.textContent = q[i].options[index]
+    })
 }
+
+// test(quiz, 0)
+
+
+b1.addEventListener(`click`, function() {
+    test(quiz, 0)
+})
+// var started = false;
 
 
 // var quizIndex = 0;
@@ -78,8 +80,7 @@ var startQuiz = () => {
 //     console.log('l;kj')
 //     const choice = event.target.innerHTML;
 //     if (started) {
-//         setTime()
-//         // var currentQuestion = quiz[quizIndex];
+//         var currentQuestion = quiz[quizIndex];
 //         if (choice === correctAnswer) {
 //             score += 25;
 //         }
@@ -100,56 +101,54 @@ var startQuiz = () => {
 //         b3.innerHTML = quiz[0].options[2]
 //         b4.innerHTML = quiz[0].options[3]
 //         correctAnswer = quiz[0].answer
-//         started = false;
+//         started = true;
 //     }
 //     // const choice = event.target.className;
 // }
 
-function updateTimer() {
-    timer.textContent = `Time left: ${secondsLeft}`
-}
+// function updateTimer() {
+//     timer.textContent = `Time left: ${secondsLeft}`
+// }
 
-var secondsLeft = 10;
-function setTime() {
-    var stopTimerID = window.setInterval(function () {
-        secondsLeft--;
-        updateTimer();
-        console.log(`${secondsLeft}`)
-        if (secondsLeft <= 0) {
-            clearInterval(stopTimerID)
-            secondsLeft = 10;
-            timerFailState();
+// var secondsLeft = 10;
+// function setTime() {
+//     var stopTimerID = window.setInterval(function () {
+//         secondsLeft--;
+//         updateTimer();
+//         console.log(`${secondsLeft}`)
+//         if (secondsLeft <= 0) {
+//             clearInterval(stopTimerID)
+//             secondsLeft = 10;
+//             timerFailState();
 
-        
-        }
-    }, 1000)
-    return stopTimerID
-    
-}
 
-function timerFailState() {
-    quizBox.innerHTML = `
-    <h1 id="question">Sorry your time has run out!</h1>
-    <button class="b1" id="">Please Click Here to Start Over</button>
-    `
-    b1.removeEventListener(`click`, checkAnswer)
-    b1.addEventListener(`click`, checkAnswer)
-}
+//         }
+//     }, 1000)
+//     return stopTimerID
 
-var showresults = () => {
-    quizBox.innerHTML = `
-    <h1 id="question">Your Score is ${score}</h1>
-    <button class="b1" id="">Please Click Here to Start Over</button>
-    <form action="post"></form>
-    `
-}
+// }
 
-// b1.addEventListener(`click`, checkAnswer)
+// function timerFailState() {
+//     quizBox.innerHTML = `
+//     <h1 id="question">Sorry your time has run out!</h1>
+//     <button class="b1" id="">Please Click Here to Start Over</button>
+//     `
+//     b1.addEventListener(`click`, startQuiz)
+// }
+
+// var showresults = () => {
+//     quizBox.innerHTML = `
+//     <h1 id="question">Your Score is ${score}</h1>
+//     <button class="b1" id="">Please Click Here to Start Over</button>
+//     <form action="post"></form>
+//     `
+// }
+
+
 // b2.addEventListener(`click`, checkAnswer)
 // b3.addEventListener(`click`, checkAnswer)
 // b4.addEventListener(`click`, checkAnswer)
 
 // console.log(currentQuestion)
 
-startQuiz();
 
