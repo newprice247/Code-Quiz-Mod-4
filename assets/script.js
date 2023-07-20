caf7c62382e99d8fd8fe91dc2573f93ef9f9052d
newprice247 <newprice247@gmail.com>
2023-07-20 18:40:29 -0400

    add answer confirmation

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 
 
 var timer = document.getElementById(`timer`);
-
+var startNext = document.getElementById(`startNext`)
 var b1 = document.querySelector(`#b1`)
 var b2 = document.querySelector(`#b2`)
 var b3 = document.querySelector(`#b3`)
@@ -22,33 +22,32 @@ console.log(quizButtons)
 const quiz = [
     {
         question: "What is the output of the following code? console.log(10 % 3);",
-        options: ["10", "1", "3", "0"],
-        answer: "1"
+        options: ["A: 10", "B: 1", "C: 3", "D: 0"],
+        answer: "B: 1"
     },
     {
         question: "Which function is used to print text to the console in JavaScript?",
-        options: ["console.log()", "print()", "document.write()", "alert()"],
-        answer: "console.log()"
+        options: ["A: console.log()", "B: print()", "C: document.write()", "D: alert()"],
+        answer: "A: console.log()"
     },
     {
         question: "What does the '+' operator do when used with strings in JavaScript?",
-        options: ["Adds the strings together", "Performs mathematical addition", "Throws an error", "Converts strings to numbers"],
-        answer: "Adds the strings together"
+        options: ["A: Adds the strings together", "B: Performs mathematical addition", "C: Throws an error", "D: Converts strings to numbers"],
+        answer: "A: Adds the strings together"
     },
     {
         question: "What is the correct way to declare a variable in JavaScript?",
-        options: ["variable x;", "var x;", "x = var;", "var = x;"],
-        answer: "var x;"
+        options: ["A: variable x;", "B: var x;", "C: x = var;", "D: var = x;"],
+        answer: "B: var x;"
     }
 ];
 
 quizButtons.forEach(element => {
     element.setAttribute(`style`, `display: none;`)
 })
-b1.removeAttribute(`style`, `display: none;`)
 
 quizQuestion.textContent = `Welcome to the Code Quiz!`;
-b1.textContent = `Please Click Here to Continue`;
+startNext.textContent = `Please Click Here to Continue`;
 
 var correctAnswer;
 var quizIndex = 0;
@@ -57,18 +56,25 @@ var quizIndex = 0;
 var score = 0;
 
 var checkAnswer = (event) => {
+    b1.addEventListener(`click`, checkAnswer)
+    b2.addEventListener(`click`, checkAnswer)
+    b3.addEventListener(`click`, checkAnswer)
+    b4.addEventListener(`click`, checkAnswer)
     timer.style.display = `block`;
     console.log(`event fired + ${quizIndex}`)
     quizQuestion.style.color = `black`;
+    startNext.setAttribute(`style`, `display: none;`)
     quizButtons.forEach(element => {
         element.removeAttribute(`style`, `display: none;`)
     })
     const choice = event.target.innerHTML;
 
     if (started) {
-
         if (choice === correctAnswer) {
             score += 25;
+            answerResult.textContent = `Correct! Total Score = ${score}/100`
+        } else {
+            answerResult.innerHTML = `Wrong! The correct answer was: <br>${quiz[quizIndex].answer} <br>Total Score = ${score}/100`
         }
 
         quizIndex++
@@ -132,18 +138,20 @@ function callClearInterval() {
         console.log(`stopping: ${stopTimerID_array[i]}`)
         clearInterval(stopTimerID_array[i])
     };
-    secondsLeft = 11;
 }
 
 function reset() {
     callClearInterval();
+    secondsLeft = 11;
     started = false;
     quizIndex = 0;
+    answerResult.textContent = '';
     timer.style.display = `none`;
+    startNext.removeAttribute(`style`, `display: none;`)
     quizButtons.forEach(element => {
         element.setAttribute(`style`, `display: none;`)
     })
-    b1.removeAttribute(`style`, `display: none;`)
+
 }
 
 function timerFailState() {
@@ -151,21 +159,17 @@ function timerFailState() {
     score = 0;
     quizQuestion.textContent = `Sorry your time has run out!`
     quizQuestion.style.color = `red`
-    b1.textContent = `Click Here to Try Again.`
+    startNext.textContent = `Click Here to Try Again.`
 }
 
 var showResults = () => {
     reset();
-    quizQuestion.textContent = `Your Final Score was ${score}`
-    b1.textContent = `Click Here to Try Again.`
+    quizQuestion.textContent = `Your Final Score was ${score} Score`
+    startNext.textContent = `Click Here to Try Again.`
     score = 0;
 }
 
-b1.addEventListener(`click`, checkAnswer)
-b2.addEventListener(`click`, checkAnswer)
-b3.addEventListener(`click`, checkAnswer)
-b4.addEventListener(`click`, checkAnswer)
-
+startNext.addEventListener(`click`, checkAnswer)
 // console.log(currentQuestion)
 
 
